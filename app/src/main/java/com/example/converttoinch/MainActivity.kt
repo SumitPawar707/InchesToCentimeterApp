7f54ee0c5e06831c23f569552ab0e487d0e79ef1
Sumit Pawar <sp260089@gmail.com>
2025-03-01 12:02:41 +0530

    Change in Background Color

--- app/src/main/java/com/example/converttoinch/MainActivity.kt
+++ app/src/main/java/com/example/converttoinch/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.converttoinch
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,12 +12,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.w3c.dom.Text
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var view:View
     private lateinit var edt1:EditText
     private lateinit var convertBtn:Button
     private lateinit var displaytxt:TextView
+    private lateinit var floatBtn:FloatingActionButton
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +31,10 @@ class MainActivity : AppCompatActivity() {
         edt1=findViewById(R.id.edit1)
         convertBtn = findViewById(R.id.convertBtn1)
         displaytxt=findViewById(R.id.centTxt)
+        floatBtn=findViewById(R.id.floatingActionButton)
+        view=findViewById(R.id.main)
+
+        val colors= arrayOf(Color.RED,Color.BLUE,Color.CYAN,Color.GRAY,Color.GREEN,Color.YELLOW)
 
         convertBtn.setOnClickListener {
             if(!edt1.text.toString().isEmpty()){
@@ -31,5 +42,9 @@ class MainActivity : AppCompatActivity() {
                 displaytxt.text="Result : %.2f centimeters ".format(result)
             }
         }
+        floatBtn.setOnClickListener{
+            view.setBackgroundColor(colors[Random.nextInt(colors.size)])
+        }
+
     }
 }
